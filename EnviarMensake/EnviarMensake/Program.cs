e2fc81dcfab4d987cac974c14a5c55c03f615fce
alejandro861 <gmalejan861@gmail.com>
2019-12-26 14:35:57 -0600

    Commit 1
    Se envia el mensaje

---
+++ EnviarMensake/EnviarMensake/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnviarMensake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensaje;
+            Console.WriteLine("Digite el mensaje que desea enviar...");
+            mensaje = Console.ReadLine();
+            Console.WriteLine("{0}",mensaje);
+        }
+    }
+}
