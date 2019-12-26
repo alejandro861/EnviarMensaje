53c95743f85ab5bdda33ce43da909ab7b3695a2d
alejandro861 <gmalejan861@gmail.com>
2019-12-26 14:46:23 -0600

    Commit 2
    Se envia destino

--- EnviarMensake/EnviarMensake/Program.cs
+++ EnviarMensake/EnviarMensake/Program.cs
@@ -7,9 +7,13 @@ namespace EnviarMensake
         static void Main(string[] args)
         {
             string mensaje;
+            string destino;
             Console.WriteLine("Digite el mensaje que desea enviar...");
             mensaje = Console.ReadLine();
             Console.WriteLine("{0}",mensaje);
+            Console.WriteLine("Digite el nombre del destinatario...");
+            destino = Console.ReadLine();
+            Console.WriteLine("Enviando mensaje '{0}' a su destino '{1}'", mensaje, destino);
         }
     }
 }
